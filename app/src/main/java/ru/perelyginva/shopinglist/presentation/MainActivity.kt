189c0c3f404c079d5f0917a9d7741df08b8e93a8
ViacheslavA <perelyginva@gmail.com>
2023-05-01 17:52:43 +0300

    Create project

---
+++ app/src/main/java/ru/perelyginva/shopinglist/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.perelyginva.shopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.perelyginva.shopinglist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
